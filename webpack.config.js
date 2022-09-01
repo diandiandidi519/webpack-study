dae29d2bb4729224afd1b1f7ba206cd1d4428eae
wangshuya <wangshuya.wsy@bytedance.com>
2022-09-01 15:42:47 +0800

    esm 调试

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const isProduction = process.env.NODE_ENV == "production";
+
+const config = {
+  entry: "./src/index.js",
+  // devtool: "eval-source-map",
+  output: {
+    filename: "[name].js",
+    chunkFilename: "[name].[contenthash:8].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    open: true,
+    host: "localhost",
+  },
+  plugins: [
+    // new HtmlWebpackPlugin({
+    //   template: "index.html",
+    // }),
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+    rules: [],
+  },
+  // optimization: {
+  //   usedExports: true,
+  // },
+  optimization: {
+    // 为了方便阅读理解打包后的代码，关闭代码压缩和模块合并
+    minimize: false,
+    concatenateModules: false,
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+  } else {
+    config.mode = "development";
+  }
+  return config;
+};
